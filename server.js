055a17ba248512aabb0891e952fe2a37932150cd
larrybabb <larry.babb@gmail.com>
2014-11-17 16:30:52 +0000

    mongoose

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var variantModel = require("./models/Variant");
 
 var app = express();
 
@@ -7,8 +9,24 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/variants', function(req, res) {
+    mongoose.model('Variant').find({}).exec(function(error,collection) {
+        res.send(collection);
+    })
+});
+
 app.get('*', function(req, res) {
     res.render('index');
+});
+
+//mongoose.connect('mongodb://localhost/variantresolver');
+mongoose.connect('mongodb://psdev:psdev@ds053320.mongolab.com:53320/variantresolver')
+
+var con = mongoose.connection;
+
+con.once('open', function() {
+    console.log('connected to mongodb successfully!');
+    variantModel.seedVariants();
 });
 
 //app.listen(3000);  // if local this is good enough
